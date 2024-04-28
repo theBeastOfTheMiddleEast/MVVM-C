240cd30cb6c91d1eed941f73f78344ce667a6315
Mert Ahmet Güneş <thebeastdigitalventures@gmail.com>
2024-04-28 17:20:23 +0300

    Initialized project

---
+++ MVVM-C/Delegates/SceneDelegate.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+            guard let windowScene = (scene as? UIWindowScene) else { return }
+            
+            let rootViewController = UIViewController()
+            rootViewController.view.backgroundColor = .systemRed
+            
+            window = UIWindow(frame: windowScene.screen.bounds)
+            window?.rootViewController = rootViewController
+            window?.windowScene = windowScene
+            window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+    func sceneWillResignActive(_ scene: UIScene) { }
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
